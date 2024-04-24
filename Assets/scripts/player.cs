63d03c4a3927124f972e282738ffa2610470ad46
lixiaok <lixiaok@uw.edu>
2024-04-24 01:12:02 -0700

    Archer Walk animation
    Made archer walking animation

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -16,36 +16,41 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        
+
         Vector3 pos = transform.position;
+        bool isWalking = false;
 
         if (Input.GetKey(KeyCode.W))
         {
             pos.y += speed * Time.deltaTime;
-            //animator.SetBool("run", true);
+            isWalking = true;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             pos.y -= speed * Time.deltaTime;
+            isWalking = true;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             GetComponent<SpriteRenderer>().flipX = false;
             pos.x += speed * Time.deltaTime;
+            isWalking = true;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             GetComponent<SpriteRenderer>().flipX = true;
             pos.x -= speed * Time.deltaTime;
+            isWalking = true;
         }
 
+        animator.SetBool("walking", isWalking);
         transform.position = pos;
-        
-        
-        
+
+
+
     }
 
 
